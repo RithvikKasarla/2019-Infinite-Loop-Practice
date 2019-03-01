d0c28d25f666abb1ec3e3d5fd248be025613432e
Arnav Borborah <arnavborborah11@gmail.com>
2019-03-01 01:07:55 +0000

    Do some shortening

--- arnavs_one_through_ten/problem_three/problem_three.py
+++ arnavs_one_through_ten/problem_three/problem_three.py
@@ -1,15 +1,10 @@
 with open("Prob03.in.txt") as input_file:
-    lines = input_file.readlines()[1:]
-
-    for line in lines:
-        num = line.strip()[:-2]
-
+    for line in input_file.readlines()[1:]:
+        num, suffix = line.strip()[:-2], "th"
         if num[-1] == "1" and num != "11":
             suffix = "st"
         elif num[-1] == "2" and num != "12":
             suffix = "nd"
         elif num[-1] == "3" and num != "13":
             suffix = "rd"
-        else:
-            suffix = "th"
         print(num + suffix)
