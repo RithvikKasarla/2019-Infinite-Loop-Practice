e93cfbebcbed4949505b566d6eebfc03b7402656
Rithvik <rithvikrk719@gmail.com>
2019-03-03 15:13:14 +0000

    Hard Coded problem 6

---
+++ rithvik_one_through_ten/Problem_Six/problem_six.py
@@ -0,0 +1,19 @@
+with open("probinput.txt") as inputFile:
+    lines = inputFile.readline().strip()
+    for line in lines:
+        line = inputFile.readline().strip()
+        statuses = line.split(" ")
+        value = 8
+        code = 0
+        errorCode = {0:"off", 1:"red", 2:"green", 3:"blue"}
+        for status in statuses:
+            if status == "BROKEN":
+                code = code + value
+            value = value / 2
+        left = "off"
+        right = "off"
+        lval = code // 4
+        rval = code % 4
+        right = errorCode[rval]
+        left = errorCode[lval]
+        print(left + " " + right)
