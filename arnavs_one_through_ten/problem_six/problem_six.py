0317af86cf4f05801fb96f45bae25950b1bf894b
Arnav Borborah <arnavborborah11@gmail.com>
2019-03-03 15:41:44 +0000

    Sum starts at 0 by default

--- arnavs_one_through_ten/problem_six/problem_six.py
+++ arnavs_one_through_ten/problem_six/problem_six.py
@@ -7,5 +7,5 @@ led_states = {
 
 with open("Prob06.in.txt") as input_file:
     for line in input_file.readlines()[1:]:
-        total = sum((v if s == "BROKEN" else 0 for (s, v) in zip(line.strip().split(), [1, 2, 4, 8])), 0)
+        total = sum((v if s == "BROKEN" else 0 for (s, v) in zip(line.strip().split(), [1, 2, 4, 8])))
         print("{} {}".format(led_states[(total - total % 4) / 4], led_states[total % 4]))
