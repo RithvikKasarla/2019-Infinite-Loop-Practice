544dac674cec63cb56d299542165bdbb9a75fd09
Arnav Borborah <arnavborborah11@gmail.com>
2019-03-07 21:15:34 +0000

    Shorten 11 code

--- arnavs_solutions/problem_eleven/problem_eleven.py
+++ arnavs_solutions/problem_eleven/problem_eleven.py
@@ -1,15 +1,6 @@
 from math import sqrt
 
 with open("Prob11.in.txt") as input_file:
-    lines = input_file.readlines()[1:]
-
-    for line in lines:
+    for line in input_file.readlines()[1:]:
         Cr, Cg, Cb, tolerance, Fr, Fg, Fb, Br, Bg, Bb = map(int, line.strip().split())
-
-        distance_background = sqrt((Br - Cr)**2 + (Bg - Cg)**2 + (Bb - Cb)**2)
-        distance_foreground = sqrt((Fr - Cr)**2 + (Fg - Cg)**2 + (Fb - Cb)**2)
-
-        if distance_foreground <= tolerance:
-            print(Br, Bg, Bb)
-        else:
-            print(Fr, Fg, Fb)
+        print(Br, Bg, Bb) if sqrt((Fr - Cr)**2 + (Fg - Cg)**2 + (Fb - Cb)**2) <= tolerance else print(Fr, Fg, Fb)
