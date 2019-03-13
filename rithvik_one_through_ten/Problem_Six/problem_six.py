328fd2abfb04dd450cb420a0e0365bb76e7447a2
Rithvik <rithvikrk719@gmail.com>
2019-03-13 23:43:16 +0000

    test for prob 6

--- rithvik_one_through_ten/Problem_Six/problem_six.py
+++ rithvik_one_through_ten/Problem_Six/problem_six.py
@@ -1,19 +1,22 @@
+def machine(line):
+    statuses = line.split(" ")
+    value = 8
+    code = 0
+    errorCode = {0:"off", 1:"red", 2:"green", 3:"blue"}
+    for status in statuses:
+        if status == "BROKEN":
+            code = code + value
+        value = value / 2
+    left = "off"
+    right = "off"
+    lval = code // 4
+    rval = code % 4
+    right = errorCode[rval]
+    left = errorCode[lval]
+    print(left + " " + right)
+    
 with open("probinput.txt") as inputFile:
     lines = inputFile.readline().strip()
     for line in lines:
         line = inputFile.readline().strip()
-        statuses = line.split(" ")
-        value = 8
-        code = 0
-        errorCode = {0:"off", 1:"red", 2:"green", 3:"blue"}
-        for status in statuses:
-            if status == "BROKEN":
-                code = code + value
-            value = value / 2
-        left = "off"
-        right = "off"
-        lval = code // 4
-        rval = code % 4
-        right = errorCode[rval]
-        left = errorCode[lval]
-        print(left + " " + right)
+        machine(line)
