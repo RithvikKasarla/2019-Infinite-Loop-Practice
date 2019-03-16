285c9a498cbf701a36eb14e187798db1df933fb2
Rithvik <rithvikrk719@gmail.com>
2019-03-16 21:48:09 +0000

    pytest 8

--- rithvik_one_through_ten/Problem_Eight/problem_eight.py
+++ rithvik_one_through_ten/Problem_Eight/problem_eight.py
@@ -1,15 +1,18 @@
-with open("ProbInput.txt") as input_file:
-    lines = input_file.readlines()[1:]
-for line in lines:
+def apollo(line):
     line = line.strip()
     li = line.split(" ")
-    for[x in li x = x.split(".")]
+    for x in li:
+        x = x.split(".")
     for x in range(0,len(li),2):
         if int(x) + (int(x+1) /100) >= 180:
             li[x] = int(x) + (int(x+1) /100) - 180
         else:
             li[x] = int(x) + (int(x+1) /100) + 180
     print(li)
-            
-            
+    
+    
+with open("ProbInput.txt") as input_file:
+    lines = input_file.readlines()[1:]
+for line in lines:
+    apollo(line)
     
---
+++ rithvik_one_through_ten/Problem_Eight/test_eight.py
@@ -0,0 +1,9 @@
+import pytest
+import problem_eight as p8
+
+lines = (("356.69 163.42 346.67","176.69 343.42 166.67"),("302.26 345.92 011.79","122.26 165.92 191.79"))
+
+@pytest.mark.parameterize("x","y")
+
+def test_ap0llo(x,y):
+    assert(apollo(x)) == y
