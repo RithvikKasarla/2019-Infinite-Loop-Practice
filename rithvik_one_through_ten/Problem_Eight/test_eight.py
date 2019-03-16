d568aeb0ceb3475087e826e1bbc06d3db378d586
Rithvik <rithvikrk719@gmail.com>
2019-03-16 21:49:16 +0000

    pytest 8

--- rithvik_one_through_ten/Problem_Eight/test_eight.py
+++ rithvik_one_through_ten/Problem_Eight/test_eight.py
@@ -5,5 +5,5 @@ lines = (("356.69 163.42 346.67","176.69 343.42 166.67"),("302.26 345.92 011.79"
 
 @pytest.mark.parameterize("x","y")
 
-def test_ap0llo(x,y):
-    assert(apollo(x)) == y
+def test_apollo(x,y):
+    assert(p8.apollo(x)) == y
