7edbb63f4b684a984134c6ced9f3dd706aeff7ba
Rithvik <rithvikrk719@gmail.com>
2019-03-17 02:14:11 +0000

    test 11 fix

--- rithvik_one_through_ten/Problem_Eleven/test_eleven.py
+++ rithvik_one_through_ten/Problem_Eleven/test_eleven.py
@@ -5,4 +5,4 @@ nums = (("0 176 80 30 12 184 90 132 101 76","132 101 76"),("0 176 80 10 12 184 9
 
 @pytest.mark.parameterize("x","y")
 def test_chrom(x,y):
-    assert(chrom(x)==y)
+    assert(p11.chrom(x)==y)
