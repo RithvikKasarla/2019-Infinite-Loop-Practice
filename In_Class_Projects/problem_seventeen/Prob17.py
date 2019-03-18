28a1493c04c7256f3eb125a5ee757994f5816954
Rithvik <rithvikrk719@gmail.com>
2019-03-18 00:00:54 +0000

    17 part

--- In_Class_Projects/problem_seventeen/Prob17.py
+++ In_Class_Projects/problem_seventeen/Prob17.py
@@ -2,10 +2,10 @@ import math
 
 def Winner(s):
     d = {"X":1,"O":0,"-":-100}
-    fline= s.split()
-    line = fline[0]
+    line= list(s)
     sk = int(math.sqrt(len(line)))
-    table = [[d[line[i]]] * sk for i in range(sk)]
+    table = [ s[i: i + 3] for i in range(0,i,3) ]
+    print(table)
     sumvert1 = 0
     sumvert2 = 0
     sumvert3 = 0
@@ -15,32 +15,32 @@ def Winner(s):
             sumhorz += int(y)
             
         if sumhorz == 3:
-            return(s,"= X Wins")
+            return(s,"= X WINS")
         elif sumhorz == 0:
-            return(s,"= Y Wins")
+            return(s,"= Y WINS")
         sumvert1 += x[0]
         sumvert2 += x[1]
         sumvert3 += x[2]
         
     if sumvert1 * sumvert3 * sumvert2%3 == 0:
-        return(s,"= X Wins")
+        return(s," = X WINS")
     elif sumvert1 * sumvert3 * sumvert2 == 0:
-        return(s,"= O Wins")
+        return(s," = O WINS")
     
     if table[0][0] + table[1][1] + table[2][2] == 3:
-        return(s,"= X Wins")
+        return(s," = X WINS")
     elif table[0][0] + table[1][1] + table[2][2] == 0:
-        return(s,"= O Wins")
+        return(s," = O Wins")
 
     if table[2,0] + table[1][1] + table[0,2] == 3:
-        return(s,"= X Wins")
+        return(s," = X WINS")
     elif table[2,0] + table[1][1] +table[0][2] == 0:
-        return(s,"= O Wins")
+        return(s," = O WINS")
         
-    return(s,"= Tie")
+    return(s,"= TIE")
     
     
 with open("input.txt") as inputFile:
     inp = inputFile.readlines()[1:]
     for z in inp:
-        print(Winner(z.strip()))
+        print ''.join(Winner(z.strip()))
