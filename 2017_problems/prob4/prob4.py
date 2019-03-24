fda961b17831c8601f2ddd6d41258d7b31150ab0
Sagar Patil <sagarreddypatil@gmail.com>
2019-03-24 19:44:27 -0400

    commit

--- 2017_problems/prob4/prob4.py
+++ 2017_problems/prob4/prob4.py
@@ -1,6 +1,6 @@
 def sort(lis):
-    for x in lis:
-        x = int(x.strip())
+    for x in range(len(lis)):
+        lis[x] = int(lis[x].strip())
     return sorted(lis)
 
 def create_fib(line):
@@ -10,12 +10,23 @@ def create_fib(line):
     dump = 0
     count = 3
     lo = line
+    lin = []
+    if lo[0] == 1:
+        lin.append("1 = 0")
+        lo.pop(0)
+    if lo[0] == 2:
+        lin.append("2 = 1")
+        lo.pop(0)
+
     while len(lo) != 0:
         z = x + y
         x = y
         y = z
-
-        if 
+        print(z, "=",  count)
+        if count in line:
+            lin.append(str(lo.pop(0))+ " = "+ str(z))
+        count += 1
+    return(lin)
 
 
 
@@ -23,4 +34,6 @@ def create_fib(line):
 with open("input.txt") as file:
     lines = file.readlines()[1:]
     spots = sort(lines)
-    print(create_fib(spots))
+    l = create_fib(spots)
+    for x in l:
+        print(x)
