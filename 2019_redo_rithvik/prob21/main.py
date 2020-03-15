47cf60ec10267a6318a05a3a795c84efe9cef7e5
RithvikKasarla <rithvikrk719@gmail.com>
2020-03-14 21:03:15 -0400

    Fixed Problem 21

--- 2019_redo_rithvik/prob21/main.py
+++ 2019_redo_rithvik/prob21/main.py
@@ -4,8 +4,8 @@ for x in range(num_tests):
     l1 = input()
     l1= l1.strip().split(" ")
     walls = []
-    for x in range( l1[len(l1)-1] ):
-        walls.append(input().strip())
+    for x in range( int(l1[len(l1)-1]) ):
+        walls.append( list(map(int, input().strip().split(" ")))) 
     
     person = (int(l1[0]),int(l1[1]))
     camera = (int(l1[2]),int(l1[3]))
@@ -20,7 +20,7 @@ for x in range(num_tests):
 
     WallEq = []
     for wall in walls:
-        w = wall.strip().split(" ") 
+        w = wall
         wallS = (int(w[0]),int(w[1]))
         WallE = (int(w[2]),int(w[3]))
         denomWall = (WallE[0]-wallS[0])
@@ -55,10 +55,10 @@ for x in range(num_tests):
             wall_points = walls[idx]
 
             if ((point[0] > wall_points[0] and point[0] < wall_points[2]) or (point[0] <= wall_points[0] and point[0] >= wall_points[2])) and ((point[1] > wall_points[1] and point[1] < wall_points[3]) or (point[1] <= wall_points[1] and point[1] >= wall_points[3])):
-                print("YES")
+                print("NO")
                 intersects = True
             else:
                 continue
 
     if not intersects:
-        print("No")
+        print("YES")
